d7163ef540138f15626d5e3ca5075d32d5df985d
lmaoken <89096442+lmaoken@users.noreply.github.com>
2022-04-08 01:42:53 -0400

    update

--- js/site.js
+++ js/site.js
@@ -1,6 +1,19 @@
 const hamburgerMenu = document.querySelector('#hamburgerMenu');
 const nav = document.querySelector('.nav');
 
+const navItem = document.querySelectorAll('.nav__item');
+const dropDown = document.querySelector('.dropdown__nav');
+
 hamburgerMenu.addEventListener('click', () => {
   nav.classList.toggle('show');
+
+  // hamburgerMenu toggle animation
+  hamburgerMenu.classList.toggle('animate');
 });
+
+// loop through all navItem and add event listener to each
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener('click', () => {
+    dropDown.classList.toggle('show');
+  });
+}
